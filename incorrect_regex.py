b71c1ed6a1656478920cacfed65417dc1063d8d5
vkp3012 <vivekpatel301295@gmail.com>
2021-09-18 22:49:02 +0530

    Add Incorrect Regex Question

---
+++ incorrect_regex.py
@@ -0,0 +1,20 @@
+"""
+You are given a string S.
+Your task is to find out 
+whether S is a valid regex or not.
+
+Input: ------------
+The first line contains integer T , the number of test cases.
+The next T lines contains the string S.
+"""
+import re
+for _ in range(int(input().split())):
+    try:
+        re.match(input(),"")
+        print("True")
+    except:
+        print("False")
+
+
+
+
