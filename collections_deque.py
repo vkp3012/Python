ddc9e78d51e7a877d11c4829c37479df69dfc81f
vkp3012 <vivekpatel301295@gmail.com>
2021-09-18 22:23:36 +0530

    Add Example and Question

--- collections_deque.py
+++ collections_deque.py
@@ -7,6 +7,30 @@ It can be used to add or remove elements from both ends.
 Deques support thread safe, memory efficient appends 
 and pops from either side of the deque with approximately the same  
 O(1) performance in either direction.
+
+Example :- 
+from collections import deque
+
+d = deque()
+d.append(1)
+d.appendleft(2)
+d.clear()
+d.extendleft('432')
+d.count('1')
+d.pop()
+d.popleft()
+d.extend('7896')
+d.remove("3")
+d.reverse()
+d.rotate(7)
+print(d)
+
+Question:-
+
+Input Format
+
+The first line contains an integer N, the number of operations.
+The next N lines contains the space separated names of methods and their values.
 """
 
 
