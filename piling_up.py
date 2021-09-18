864b3f48e63ca56ce05e729223d6ee0a10ba8133
vkp3012 <vivekpatel301295@gmail.com>
2021-09-18 20:15:14 +0530

    Add piling up

---
+++ piling_up.py
@@ -0,0 +1,18 @@
+"""
+The first line contains a single integer T, the number of test cases.
+For each test case, there are 2 lines.
+The first line of each test case contains n, the number of cubes.
+The second line contains n space separated integers, denoting the sideLengths of each cube in that order.
+"""
+
+for t in range(int(input("Enter Number:"))):
+    input()
+    lst = [int(i) for i in input().split()]
+    min_list = lst.index(min(lst))
+    left = lst[:min_list]
+    right = lst[min_list+1:]
+    if left == sorted(left,reverse=True) and right==sorted(right):
+        print("Yes")
+    else:
+        print("No")   
+
