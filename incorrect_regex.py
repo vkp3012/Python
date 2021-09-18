9b32fcce31417ed9faf9499fc2f8940028d7a777
vkp3012 <vivekpatel301295@gmail.com>
2021-09-18 22:57:36 +0530

    Modify Codding

--- incorrect_regex.py
+++ incorrect_regex.py
@@ -8,12 +8,27 @@ The first line contains integer T , the number of test cases.
 The next T lines contains the string S.
 """
 import re
+
+"""
 for _ in range(int(input().split())):
     try:
         re.match(input(),"")
         print("True")
     except:
         print("False")
+
+"""
+
+n = input()
+
+for i in range(int(n)):
+    key = input()
+    try:
+        re.compile(key)
+        is_valid = True
+    except re.error:
+        is_valid = False
+    print(is_valid)
 
 
 
