781bc2eea2fc6a9de5fc2c0a416b3cf20cc81920
vkp3012 <vivekpatel301295@gmail.com>
2021-09-23 17:48:14 +0530

    Add Collections namedtuple file

---
+++ collections_namedtuple.py
@@ -0,0 +1,16 @@
+"""
+collections.namedtuple()
+
+Basically, namedtuples are easy to create, lightweight object types.
+They turn tuples into convenient containers for simple tasks.
+With namedtuples, you don’t have to use integer indices for accessing members of a tuple.
+"""
+
+from collections import namedtuple
+"""
+point = namedtuple("point",'x,y')
+pt1 = point(1,2)
+pt2 = point(3,4)
+dot_product = (pt1.x*pt2.x) + (pt1.y*pt2.y)
+print(dot_product)
+"""
